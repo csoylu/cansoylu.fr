2a76388ebb06f565d2d3898268bf44a7b8ec170f
Can SOYLU <soylucan@hotmail.fr>
2022-11-22 01:11:43 +0100

    user creation

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 //Schema for facebook user
 const userSchema = mongoose.Schema({
-    name: String,
+    _id: String,
     password: String,
-    id: String,
+    date: Date,
+    name: String,
+    profilePic: String,
 });
 
 module.exports = mongoose.model('User', userSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,34 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-    console.log('Create User GET /');
-    res.send('in user');
-})
 
-router.get('/test', (req, res) => {
-    console.log('Create User GET /test');
+// Password encryption
+const saltRounds = 10;
+
+
+
+router.get('/login', (req, res) => {
+    console.log('login');
     res.send('in user test');
 })
 
 router.post('/create', (req, res) => {
-    console.log(req.body);
-    
-    const user = new User({
-        name: req.body.name,
-        password: req.body.password,
-        id: req.body.id,
+    User.find({_id: req.body.id}, (err, user) => {
+        if (user.length > 0) {
+            console.log(`Cannot create user ${req.body.id} already exists`);
+            res.send(false);
+        } else {
+            console.log(`Creating user ${req.body.id}`);
+            const user = new User({
+                _id: req.body.id,
+                name: req.body.name,
+                date: Date.now(),
+                profilePic: "",
+            });
+            user.save()
+            .catch(err => {
+                res.send("Error: " + err);
+                console.log(err);
+            });
+
+            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+                // Store hash in your password DB.
+                user.password = hash;
+                user.save()
+                .catch(err => {
+                    res.send("Error: " + err);
+                    console.log(err);
+                });
+            });
+            res.send(true);
+        }
     });
-
-    user.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({message: err});
-        });
 })
-
 
 module.exports = router;
